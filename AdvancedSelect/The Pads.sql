b5721c0bb77d19acbceb7bf8f2e2eb47d38e71ad
b-rtn <36545623+b-rtn@users.noreply.github.com>
2019-05-20 17:22:01 -0500

    Add files via upload

---
+++ AdvancedSelect/The Pads.sql
@@ -0,0 +1,9 @@
+
+select concat(name,'(',left(occupation, 1),')')
+from occupations
+order by name;
+
+select concat('There are a total of ',count(occupation),' ',lower(occupation),'s.')
+from occupations
+group by occupation
+order by count(occupation), occupation;
